35cefff2d698d3f119c7e829fde22e0fcc822602
Arctiqa <Sinitro@yandex.ru>
2024-02-26 14:11:50 +0800

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from datetime import datetime
 
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 def index(request):
+    products = Product.objects.all()
     latest_products = Product.objects.order_by('-created_at')[:5]
 
-    return render(request, 'catalog/index.html',  {'latest_products': latest_products})
+    context = {
+        'object_list': products,
+        'title':  'Главная',
+        'latest_products': latest_products
+    }
+
+    return render(request, 'catalog/index.html',  context)
 
 
 def contacts(request):
@@ -15,8 +22,23 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
         with open('log.txt', 'a', encoding='utf-8') as f:
             f.write(f'{datetime.now()} {name} ({phone}): {message}\n')
 
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Контакты',
+    }
+
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+
+    product_item = Product.objects.get(pk=pk)
+
+    context = {
+        'product': product_item,
+        'title': product_item.product_name
+
+    }
+    return render(request, 'catalog/product.html', context)
