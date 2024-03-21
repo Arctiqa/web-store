0bc50626f1219b06823c51d5508339dce4a2eb3e
Arctiqa <Sinitro@yandex.ru>
2024-03-21 22:09:07 +0800

    update urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from catalog.apps import MainConfig
-from catalog.views import IndexListView, ProductDetailView, ContactsTemplateView
+from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView, ProductCreateView, \
+    ProductUpdateView, ProductDeleteView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', IndexListView.as_view(), name='index'),
+    path('', ProductListView.as_view(), name='index'),
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product')
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
+    path('create', ProductCreateView.as_view(), name='create'),
+    path('edit/<int:pk>', ProductUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>', ProductDeleteView.as_view(), name='delete')
 
 ]
