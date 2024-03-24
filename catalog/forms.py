991c867631d17a88209c53e368c18a95a046ffe4
Arctiqa <Sinitro@yandex.ru>
2024-03-25 00:45:29 +0800

    added VersionForm and MixinFormControl

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,24 +1,40 @@
 from django import forms
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
-
-    class Meta:
-        model = Product
-        fields = '__all__'
-        # exclude = ('version')
-
+class MixinFormControl:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
+
+class ProductForm(MixinFormControl, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        # exclude = ('version')
+
     def clean_product_name(self):
         product_name = self.cleaned_data['product_name']
 
-        if product_name in ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']:
+        if product_name in ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                            'радар']:
             raise forms.ValidationError('Слово запрещено в названии продукта')
         return product_name
+
+
+class VersionForm(MixinFormControl, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
+
+    def clean_version_name(self):
+        valid_version = self.cleaned_data['version']
+
+        if valid_version < 0:
+            raise forms.ValidationError('Номер версии не может быть отрицательным')
+        return valid_version
+
 
