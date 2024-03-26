865870e077599dfd704b89a1d45cfd068af1d8cd
Arctiqa <Sinitro@yandex.ru>
2024-03-27 00:19:56 +0800

    update user/models.py

--- users/models.py
+++ users/models.py
@@ -9,8 +9,15 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatars/', default='avatars/default.jpg', verbose_name='аватар')
+    telegram = models.CharField(max_length=150, verbose_name='телеграм', **NULLABLE)
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=40, verbose_name='страна', **NULLABLE)
+    token = models.CharField(max_length=50, verbose_name='токен')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
