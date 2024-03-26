04c0d6b3731908ffe976fbfa6671ebfcf6a8508a
Arctiqa <Sinitro@yandex.ru>
2024-03-27 00:21:47 +0800

    update urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, verify, ResetPasswordView, \
+    NewPassGenerateView
 
 app_name = UsersConfig.name
 
@@ -11,5 +12,8 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile')
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('verify/<str:token>', verify, name='verify'),
+    path('reset_password/', ResetPasswordView.as_view(), name='reset_password'),
+    path('new_pass_generate/', NewPassGenerateView.as_view(), name='new_pass_generate')
 ]
