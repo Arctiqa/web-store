e149627514a1a059579aae80211aa149c8383637
Arctiqa <Sinitro@yandex.ru>
2024-03-27 00:20:55 +0800

    update forms.py added ResetPasswordForm

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,19 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.core.mail import EmailMultiAlternatives
+
+from catalog.forms import MixinFormControl
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordResetForm, SetPasswordForm
 
 from users.models import User
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(MixinFormControl, UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
 
 
-class UserProfileForm(UserChangeForm):
+class UserProfileForm(MixinFormControl, UserChangeForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'phone', 'country', 'avatar')
@@ -18,3 +21,22 @@ class UserProfileForm(UserChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['password'].widget = forms.HiddenInput()
+
+
+class ResetPasswordForm(MixinFormControl, PasswordResetForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+        except Exception:
+            raise forms.ValidationError('Пользователь с такой почтой не зарегистрирован')
+
+        return email
+
+    def send_mail(self, subject_template_name, email_template_name, context, from_email, to_email,
+                  html_email_template_name=None):
+        pass
