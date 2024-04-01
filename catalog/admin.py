cb1e55aa7a8fdb07e12296e2eaccbacdc10df736
Arctiqa <Sinitro@yandex.ru>
2024-04-01 17:16:53 +0800

    update admin.py from catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,25 +7,21 @@ from catalog.models import Product, Category, Version
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'category_name', 'description')
     list_filter = ('category_name',)
-    list_editable = ('category_name', 'description')
     search_fields = ('category_name', 'description',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product_name', 'price', 'category', 'image')
-    list_filter = ('category',)
     search_fields = ('product_name', 'description',)
 
-    fieldsets = (
-        (None, {
-            'fields': ('product_name', 'price', 'category', 'image', 'description')
-        }),
-    )
+    # fieldsets = (
+    #     (None, {
+    #         'fields': ('product_name', 'price', 'category', 'image', 'description', 'is_published')
+    #     }),
+    # )
 
 
 @admin.register(Version)
 class VersionAdmin(admin.ModelAdmin):
     list_display = ('product', 'version', 'version_name', 'is_current_version')
     list_filter = ('version_name',)
-
